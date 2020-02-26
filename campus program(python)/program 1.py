097160f5d7877b0c9529c448373edd3b050e1da0
kunaldesign <kunalhedaoo25@gmail.com>
2020-02-26 08:15:29 +0530

    updated program

--- campus program(python)/program 1.py
+++ campus program(python)/program 1.py
@@ -1,6 +1,12 @@
 #%%
-print 'hello'
-
-
-
+num=int(input('enter the positive number: '))
+if num>1:
+    for i in range(2,num//2):
+        if(num%i)==0:
+            print(num,"is not a prime number.")
+            break
+    else:
+            print(num,"is a prime number")
+else:
+    print(num,"is not a")
 # %%
